036c7dd90dba00c36282087b7a32e120a97a3fc4
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2021-12-22 21:11:04 +0200

    RELEASE

--- src/backendApi/baseBackendUrl.js
+++ src/backendApi/baseBackendUrl.js
@@ -1,4 +1,4 @@
-const publicInternIpBackendApi = 'http://192.168.0.178:8000';
+const publicInternIpBackendApi = 'http://192.168.100.6:8000';
 const localBackendApi = 'http://localhost:8000'
 const publicBackendApi = 'https://ptpcc.azurewebsites.net'
 
